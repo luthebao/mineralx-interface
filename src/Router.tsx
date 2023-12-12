f1aade21b02ddc85b0bedacb8541b7e382f07807
Quy Nguyen <quynguyen@tomochain.com>
2023-12-12 23:09:02 +0700

    feat: add home

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,16 +1,16 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./layout/layout";
 import Swap from "./pages/swap";
-
+import HomePage from "./pages/home";
 
 export default function Router() {
-
-    return (
-        <Routes>
-            <Route path='/' element={<Layout />}>
-                <Route index element={<Navigate to={"/swap"} />} />
-                <Route path='*' element={<Swap />} />
-            </Route>
-        </Routes>
-    )
+  return (
+    <Routes>
+      <Route path="/" index element={<HomePage />} />
+      <Route element={<Layout />}>
+        <Route path="/swap" element={<Swap />} />
+        <Route path="*" element={<Swap />} />
+      </Route>
+    </Routes>
+  );
 }
