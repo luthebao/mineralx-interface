5fab2a62a2376f15862fab128913ca4a84394980
Quy Nguyen <quynguyen@tomochain.com>
2023-12-13 23:38:50 +0700

    fix: css

--- src/components/Swap/SwapContainer.tsx
+++ src/components/Swap/SwapContainer.tsx
@@ -4,7 +4,7 @@ import SwapCore from './SwapCore';
 function SwapContainer() {
     return (
         <div className="flex justify-center items-center">
-            <Paper elevation={0} className="max-w-[750px] w-full p-[12px] flex flex-col bg-card">
+            <Paper elevation={0} className="max-w-[750px] w-full p-[30px] flex flex-col bg-card">
                 <SwapCore />
             </Paper>
         </div>
